6597c36157a50b314d0520ebb07f2f02b47f0403
Nirmalsitoula <nirmalsitoula97@gmail.com>
2023-07-09 08:00:41 +0545

    first commit

---
+++ flow/src/App.js
@@ -0,0 +1,16 @@
+import Header from './components/Header';
+import {BrowserRouter} from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="App">
+      
+      <Header/>
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ flow/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      
+    <h1> my name is nirmal</h1>
+    </div>
+  )
+}
+
+export default Header
